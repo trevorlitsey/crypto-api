d1680abbe32fcf8437e3b54acaa25ccac4b2c9a6
Trevor Litsey <trevorlitsey@gmail.com>
2021-03-01 21:07:41 -0600

    switch to coinbase pro

--- src/index.js
+++ src/index.js
@@ -1,62 +1,49 @@
+const CoinbasePro = require('coinbase-pro');
 require('dotenv').config();
 const CronJob = require('cron').CronJob;
 
-const kraken = require('./kraken');
 const sendText = require('./sendText');
 
-const BITCOIN = 'XBT';
+const authedClient = new CoinbasePro.AuthenticatedClient(
+  process.env.COINBASE_API_KEY,
+  process.env.COINBASE_API_SECRET,
+  process.env.COINBASE_PASSPHRASE,
+  process.env.COINBASE_API_ENDPOINT
+);
+
+const BUY = 'buy';
+const MARKET = 'market';
+const BITCOIN = 'BTC';
 const USD = 'USD';
-const PAIR = BITCOIN + USD;
+const PRODUCT_ID = BITCOIN + '-' + USD;
 const USD_TO_SPEND = 10;
 
 const buyCyrpto = async () => {
-  let orderDetails = [];
-
   try {
-    const tickerRes = await kraken.api('Ticker', {
-      pair: PAIR,
-    });
-
-    if (tickerRes.error.length) {
-      throw new Error(JSON.stringify(tickerRes.error, null, '\t'));
-    }
-
-    const currentTradingPrice = Object.values(tickerRes.result)[0].a[0];
-
     const order = {
-      pair: PAIR,
-      type: 'buy',
-      ordertype: 'limit',
-      price: currentTradingPrice,
-      volume: (USD_TO_SPEND / currentTradingPrice).toFixed(5),
+      side: BUY,
+      product_id: PRODUCT_ID,
+      funds: USD_TO_SPEND,
+      type: MARKET,
     };
 
-    orderDetails = [
-      `Volume: ${order.volume}`,
-      `Price: ${order.price}`,
-      `USD Total: $${(order.price * order.volume).toFixed(2)}`,
-    ];
-
-    const orderRes = await kraken.api('AddOrder', order);
+    const orderRes = await authedClient.placeOrder(order);
 
     console.log(JSON.stringify(orderRes, null, '\t'));
 
-    if (orderRes.error.length) {
-      throw new Error(JSON.stringify(tickerRes.error, null, '\t'));
-    }
-
     sendText(
-      [
-        'BITCOIN ORDER SUCCESSFUL',
-        ...orderDetails,
-        `Response: ${orderRes.result.descr.order}`,
-      ].join('\n')
-    );
-  } catch (e) {
-    sendText(
-      ['BITCOIN ORDER FAILED', ...orderDetails, `Error: ${e.message}`].join(
+      ['BITCOIN ORDER SUCCESSFUL', JSON.stringify(orderRes, null, '\t')].join(
         '\n'
       )
+    );
+  } catch (e) {
+    console.error(e.message);
+    sendText(
+      [
+        'BITCOIN ORDER FAILED',
+        `USD Total: $${USD_TO_SPEND}`,
+        `Error: ${e.message}`,
+      ].join('\n')
     );
   }
 };
--- src/kraken.js
+++
@@ -1,6 +0,0 @@
-const KrakenClient = require('kraken-api');
-
-module.exports = new KrakenClient(
-  process.env.KRAKEN_API_KEY,
-  process.env.KRAKEN_SECRET,
-);
